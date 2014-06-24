34cd7a7a6258ac004e36e02ac1485a994eb97c08
guerragiancarlo8 <guerragiancarlo8@gmail.com>
2014-06-23 21:24:45 -0400

    Prime factorization problem

---
+++ problem4.c
@@ -0,0 +1,30 @@
+//palindrome
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main(){
+
+	//the biggest palindrome that can be formed with the multiplication of two three digit numbers
+
+	int i;
+	int j;
+	int high_score;
+
+	char reversed_string1;
+	char reversed_string2;
+
+	for(i=999;i>0;i--){
+
+
+		for(j=999;j>0;j--){
+
+		
+
+		}
+	}
+    
+
+}
